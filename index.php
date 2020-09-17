9f6bfe75c3b9c1a34fa556c136510acc53d2871b
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-17 12:46:49 +0200

    add session_start,include to pass.php, doctype html, and connection to the database in index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include('pass.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        try
+        {
+            $bdd = new PDO('mysql:host=localhost;dbname=randomQuotes;charset=utf8', 'root', $_SESSION['pass']);
+            $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch(Exception $e)
+        {
+                die('Erreur : '.$e->getMessage());
+        }
+    ?>
+</body>
+</html>
