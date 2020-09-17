bc00041ccf109f19b3de47774f0a6b5f66610af6
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-17 20:58:03 +0200

    delete the H1, and change the class test to quote_container in index.php

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ include('pass.php');
     <title>Document</title>
 </head>
 <body>
-        <h1>Random quotes generator</h1>
 
     <?php
         try
@@ -28,7 +27,7 @@ include('pass.php');
         $get_quotes = $bdd->query('SELECT author,quote FROM quotes ORDER BY RAND() LIMIT 1');
         $get_quotes->execute();
         while($display_quotes = $get_quotes->fetch()){
-                echo '<div class="test">' . " “ " . $display_quotes['quote'] . " ” " . '</br>';
+                echo '<div class="quote_container">' . " “ " . $display_quotes['quote'] . " ” " . '</br>';
                 echo '<p class="p_author">' . ' - ' . $display_quotes['author'] .'</p>' . '</div>';
         }
     ?>
