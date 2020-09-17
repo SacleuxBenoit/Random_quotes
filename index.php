3c00e91d186edc8807192c275d7cedfacbdf51d3
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-17 12:53:12 +0200

    select author and quote from the database and display them in index.php

--- index.php
+++ index.php
@@ -21,6 +21,14 @@ include('pass.php');
         {
                 die('Erreur : '.$e->getMessage());
         }
+
+        $get_quotes = $bdd->query('SELECT author,quote FROM quotes ORDER BY RAND() LIMIT 1');
+        $get_quotes->execute();
+        while($display_quotes = $get_quotes->fetch()){
+            echo $display_quotes['author'];
+            echo $display_quotes['quote'];
+
+        }
     ?>
 </body>
 </html>
