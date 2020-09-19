a88daef1d5887903810719f990a392e511d47328
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-19 10:50:01 +0200

    add 3 div, with an image + a paragraph in each div in index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/style_index.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Random Quotes</h1>
+
+    <div>
+        <img src="./images/quotes_motivations.png" class="img_motivations">
+        <p>
+            quotes motivations
+        </p>
+    </div>
+
+    <div>
+        <img src="./images/quotes_films.png" class="img_films">
+        <p>
+            quotes films
+        </p>
+    </div>
+
+    <div>
+        <img src="./images/quotes_series.png" class="img_series">
+        <p>
+            quotes series
+        </p>
+    </div>
+</body>
+</html>
