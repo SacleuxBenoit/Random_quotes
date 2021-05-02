5d3b0b659ce1161c7e8fa04e84b0ea1d25df4684
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-02 15:06:16 +0200

    change the title + link to the css file :arrow_right: layouts/addQuotes.php

--- layouts/addQuotes.php
+++ layouts/addQuotes.php
@@ -9,8 +9,8 @@ include('../login_database.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="../css/style_addQuotes.css">
+    <title>Create quotes</title>
 </head>
 <body>
     <form action="addQuotes.php" method="post">
